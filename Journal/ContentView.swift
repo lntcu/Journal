99cc10321aa873bbb91a5c94ff6e3e607caba504
lntcu <lntcu@icloud.com>
2025-07-17 11:02:11 +0800

    Initial Commit

---
+++ Journal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Journal
+//
+//  Created by user on 17/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by user on 17/7/25.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
